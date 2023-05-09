a52e567a9eb76ab18e2d71be2515c86cdd5908f9
Caio Reis <cbrr@cesar.school>
2023-05-09 15:14:03 -0300

    User logout

--- buzzlab_app/urls.py
+++ buzzlab_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, register_request, login_request
+from .views import home, register_request, login_request, logout_request
 
 urlpatterns = [
 	path('', home),
 	path('register/', register_request),
 	path('login/', login_request),
+	path('logout/', logout_request),
 ]
